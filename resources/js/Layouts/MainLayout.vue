1dcc98c7ed31b067191c5d7a449c897d8fa13947
DevAsFahim <108362901+DevAsFahim@users.noreply.github.com>
2023-05-30 22:15:32 +0600

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+import Compass from 'vue-material-design-icons/Compass.vue';
+import SendOutline from 'vue-material-design-icons/SendOutline.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import AccountOutline from 'vue-material-design-icons/AccountOutline.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue';
+
+const showCreatePost = ref(false);
+
+</script>
+
+<template>
+    <div class="bg-red-300">
+        <h2>Main layout</h2>
+
+    </div>
+
+    <slot />
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <h1 class="text-2xl">Home Page</h1>
+        <h2>kire beda kaj korbi na keno</h2>
+    </MainLayout>
+</template>
